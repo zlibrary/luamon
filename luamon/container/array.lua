81b6fe3ee69198dab6859984f63262fa6ede6630
zlibrary <zlibrary@163.com>
2021-03-02 19:47:35 +0800

    1

--- luamon/container/array.lua
+++ luamon/container/array.lua
@@ -29,6 +29,18 @@ function __array_base:init(n)
     end
 end
 
+function __array_base:size()
+    return self.__size
+end
+
+function __array_base:get(n)
+    return self.__elems[n]
+end
+
+function __array_base:set(n, v)
+    self.__elems[n] = v
+end
+
 ---------------------------------------------------------------------
 --- 'Array'迭代器
 local __array_iterator = newclass('luamon.container.__array_iterator')
@@ -38,48 +50,81 @@ function __array_iterator(object, index)
     self.__index  = index
 end
 
-function __array_iterater:get()
+function __array_iterator:get()
     return self.__object.__elems[self.__index]
 end
 
-function __array_iteratro:set(v)
+function __array_iterator:set(v)
     self.__object.__elems[self.__index] = v
 end
 
-
-
-
-
 ---------------------------------------------------------------------
---- 'Array'迭代器
-local __array_iterator = newclass("luamon.container.__array_iterator")
-
--- 构造方法
-function __array_iterator:init(array, index)
-
-
-
-
-
----------------------------------------------------------------------
---- "Array"基础结构
-local __array_traits = newclass("__array_traits")
-
-function __array_traits:init(n)
-    local nm = tointeger(n)
-    if nm and (nm >= 0) then
-        self.__size  = nm
-        self.__elems = {}
-    else
-        error(string.format("'%s[%s]' is not valid argument for type 'unsigned int'", n, type(n)))
-    end
-end
-
----------------------------------------------------------------------
---- "Array"类型定义
-local array = newclass("array", __array_traits)
+--- 'Array'定义
+local array = newclass('luamon.container.array', __array_base)
 
 function array:init(n)
     self.super:init(n)
+end
+
+function array:size()
+    return self.super:size()
+end
+
+function array:capacity()
+    return self.super:size()
+end
+
+function array:empty()
+    return (self.super:size() == 0)
+end
+
+function array:get(n)
+    local nm = tointeger(n)
+    if (nm == nil) then
+        error(string.format("'%s[%s]' is not valid argument for type 'unsigned int'.", n, type(n)))
+    else
+        if (nm <= 0) or (nm > self.super:size()) then
+            error("out of range.")
+        else
+            return self.super:get(nm)
+        end
+    end
+end
+
+function array:set(n, v)
+    local nm = tointeger(n)
+    if (nm == nil) then
+        error(string.format("'%s[%s]' is not valid argument for type 'unsigned int'.", n, type(n)))
+    else
+        if (nm <= 0) or (nm > self.super:size()) then
+            error("out of range.")
+        else
+            return self.super:set(nm, v)
+        end
+    end
+end
+
+function array:front()
+    return self.super:get(1)
+end
+
+function array:back()
+    return self.super:get(self.super:size())
+end
+
+function array:head()
+    error("this function not implemented.")
+end
+
+function array:tail()
+    error("this function not implemented.")
+end
+
+function array:rhead()
+    error("this function not implemented.")
+end
+
+function array:rtail()
+    error("this function not implemented.")
 end
 
