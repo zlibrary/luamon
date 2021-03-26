4546b6373714cf4dffc0fcecbfcc0ee9e492ca5a
zlibrary <zlibrary@163.com>
2021-03-26 16:55:09 +0800

    红黑树插入逻辑

--- luamon/container/rbtree.lua
+++ luamon/container/rbtree.lua
@@ -11,63 +11,63 @@ local __rbtree_iterator = newclass("__rbtree_iterator", require("luamon.containe
 
 function __rbtree_iterator:init(inst, node)
     self.super:init("bidirectional")
-    self.__inst = inst
-    self.__node = node
+    self.inst = inst
+    self.node = node
 end
 
 function __rbtree_iterator:get()
-    return self.__node.value
+    return self.node.value
 end
 
 function __rbtree_iterator:set(v)
-    self.__node.value = v
+    self.node.value = v
 end
 
 function __rbtree_iterator:increment()
-    if (self.__node == self.__inst.__header) then
+    if (self.node == self.inst.header) then
         return
     else
-        if (self.__node.rchild ~= nil) then
-            self.__node = self.__node.rchild
-            while(self.__node.lchild) do
-                self.__node = self.__obj.lchild
+        if (self.node.rchild ~= nil) then
+            self.node = self.node.rchild
+            while(self.node.lchild) do
+                self.node = self.node.lchild
             end
         else
-            local p = self.__node.parent
-            local x = self.__node
+            local p = self.node.parent
+            local x = self.node
             while(x == p.rchild) do
                 x = p
                 p = p.parent
             end
-            if (x == self.__inst.__header) then
-                self.__node = x
+            if (x == self.inst.header) then
+                self.node = x
             else
-                self.__node = p
+                self.node = p
             end
         end
     end
 end
 
 function __rbtree_iterator:decrement()
-    if (self.__node == self.__inst.__header) then
-        self.__node =  self.__node.rchild
+    if (self.node == self.inst.header) then
+        self.node =  self.node.rchild
     else
-        if (self.__node.lchild ~= nil) then
-            self.__node = self.__node.lchild
-            while(self.__node.rchild) do
-                self.__node = self.__node.rchild
+        if (self.node.lchild ~= nil) then
+            self.node = self.node.lchild
+            while(self.node.rchild) do
+                self.node = self.node.rchild
             end
         else
-            local p = self.__node.parent
-            local x = self.__node
+            local p = self.node.parent
+            local x = self.node
             while(x == p.lchild) do
                 x = p
                 p = p.parent
             end
-            if (x == self.__inst.__header) then
-                self.__node = x
+            if (x == self.inst.header) then
+                self.node = x
             else
-                self.__node = p
+                self.node = p
             end
         end
     end
@@ -94,13 +94,13 @@ function __rbtree_iterator:advance(n)
 end
 
 function __rbtree_iterator:prev()
-    local tmp = __rbtree_iterator:new(self.__inst, self.__node)
+    local tmp = __rbtree_iterator:new(self.inst, self.node)
     tmp:decrement()
     return tmp
 end
 
 function __rbtree_iterator:next()
-    local tmp = __rbtree_iterator:new(self.__inst, self.__node)
+    local tmp = __rbtree_iterator:new(self.inst, self.node)
     tmp:increment()
     return tmp
 end
@@ -110,15 +110,15 @@ function __rbtree_iterator:distance(other)
 end
 
 function __rbtree_iterator:__eq(other)
-    return (self.class() == other.class()) and (self.__inst == other.__inst) and (self.__node == other.__node)
+    return (self.class() == other.class()) and (self.inst == other.inst) and (self.node == other.node)
 end
 
 function __rbtree_iterator:__add(n)
     local nm = math.tointeger(n)
     if nm and (nm >= 0) then
-        local tmp = __rbtree_iterator:new(self.__inst, self.__node)
+        local tmp = __rbtree_iterator:new(self.inst, self.node)
         tmp:advance(nm)
-        return iter
+        return tmp
     else
         error(string.format("'%s[%s]' is invalid argument for type 'unsigned int'.", tostring(n), type(n)))
     end
@@ -127,9 +127,9 @@ end
 function __rbtree_iterator:__sub(n)
     local nm = math.tointeger(n)
     if nm and (nm >= 0) then
-        local tmp = __rbtree_iterator:new(self.__inst, self.__node)
+        local tmp = __rbtree_iterator:new(self.inst, self.node)
         tmp:advance(-nm)
-        return iter
+        return tmp
     else
         error(string.format("'%s[%s]' is invalid argument for type 'unsigned int'.", tostring(n), type(n)))
     end
@@ -140,23 +140,63 @@ local __rbtree_color_red   = 1
 local __rbtree_color_black = 2
 
 local function __rbtree_begin(x)
-    return x.__header.parent
+    return x.header.parent
 end
 
 local function __rbtree_end(x)
-    return x.__header
+    return x.header
 end
 
 local function __rbtree_root(x)
-    return x.__header.parent
+    return x.header.parent
 end
 
 local function __rbtree_minimum(x)
-    return x.__header.lchild
+    return x.header.lchild
 end
 
 local function __rbtree_maximum(x)
-    return x.__header.rchild
+    return x.header.rchild
+end
+
+local function __rbtree_rotate_l(this, x)
+    local y  = x.rchild
+    x.rchild = y.lchild
+    if (y.lchild ~= nil) then
+        y.lchild.parent = x
+    end
+    y.parent = x.parent
+    if (this.header.parent == x) then
+        this.header.parent = y
+    else
+        if (x.parent.lchild == x) then
+            x.parent.lchild = y
+        else
+            x.parent.rchild = y
+        end
+    end
+    y.lchild = x
+    x.parent = y
+end
+
+local function __rbtree_rotate_r(this, x)
+    local y  = x.lchild
+    x.lchild = y.rchild
+    if (y.rchild ~= nil) then
+        y.rchild.parent = x
+    end
+    y.parent = x.parent
+    if (this.header.parent == x) then
+        this.header.parent = y
+    else
+        if (x.parent.lchild == x) then
+            x.parent.lchild = y
+        else
+            x.parent.rchild = y
+        end
+    end
+    y.rchild = x
+    x.parent = y
 end
 
 local function __rbtree_get_insert_unique_pos(this, k)
@@ -175,7 +215,7 @@ local function __rbtree_get_insert_unique_pos(this, k)
     local j = __rbtree_iterator:new(this, y)
     if c then
         -- 新键值作为'j'的左孩子插入
-        if (this.xbegin() == j) then
+        if (this:xbegin() == j) then
             -- 'y'是首节点（无需后退检查是否存在重复键值）
             return {x, y}
         else
@@ -204,31 +244,94 @@ local function __rbtree_get_insert_equal_pos(this, k)
     return {x, y}
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+local function __rbtree_insert_aux(this, p, v)
+    -- 节点插入操作
+    local linsert = (p == this.header) or (this.kcompare(this.kextract(v), this.kextract(p.value)))
+    local x = 
+    {
+        parent = p,
+        color  = __rbtree_color_red,
+        value  = v,
+    }
+    if linsert then
+        -- 作为左节点插入
+        p.lchild = x
+        if (this.header == p) then
+            this.header.parent = x
+            this.header.rchild = x
+        end
+        if (this.header.lchild == p) then
+            this.header.lchild = x
+        end
+    else
+        -- 作为右节点插入
+        p.rchild = x
+        if (this.header.rchild == p) then
+            this.header.rchild = x
+        end
+    end
+    -- 红黑树平衡
+    while((this.header.parent ~= x) and (x.color == __rbtree_color_red)) do
+        local g = x.parent.parent
+        local p = x.parent
+        local u = nil
+        if (p == g.lchild) then
+            u =  g.rchild
+        else
+            u =  g.lchild
+        end
+        if (p.color == __rbtree_color_black) then
+            break
+        end
+        if u and (u.color == __rbtree_color_red) then
+            -- 重新染色
+            g.color = __rbtree_color_red
+            p.color = __rbtree_color_black
+            u.color = __rbtree_color_black
+            x = g
+        else
+            -- 结构调整
+            if (p == g.lchild) then
+                if (x == p.rchild) then
+                    __rbtree_rotate_l(this, p)
+                    p = x
+                    x = p.lchild
+                end
+                __rbtree_rotate_r(this, g)
+                -- 重新染色
+                g.color = __rbtree_color_red
+                x.color = __rbtree_color_red
+                p.color = __rbtree_color_black
+            else
+                if (x == p.lchild) then
+                    __rbtree_rotate_r(this, p)
+                    p = x
+                    x = p.rchild
+                end
+                __rbtree_rotate_l(this, g)
+                -- 重新染色
+                g.color = __rbtree_color_red
+                x.color = __rbtree_color_red
+                p.color = __rbtree_color_black
+            end
+            x = p
+        end
+    end
+    this.header.parent.color = __rbtree_color_black
+    this.count = this.count + 1
+    return x
+end
 
 -------------------------------------------------------------------------------
 --- 红黑树
 local rbtree = newclass("rbtree", require("luamon.container.traits.container"))
 
 function rbtree:xbegin()
-    return __rbtree_iterator:new(self, self.__header.lchild)
+    return __rbtree_iterator:new(self, self.header.lchild)
 end
 
 function rbtree:xend()
-    return __rbtree_iterator:new(self, self.__header)
+    return __rbtree_iterator:new(self, self.header)
 end
 
 function rbtree:rbegin()
@@ -252,25 +355,25 @@ function rbtree:init(kcompare, kextract)
         end
     end
     self.super:init("sequential")
-    self.__kcompare = kcompare
-    self.__kextract = kextract
-    self.__count   = 0
-    self.__header  = 
+    self.kcompare = kcompare
+    self.kextract = kextract
+    self.count    = 0
+    self.header   = 
     {
         parent = nil,
         value  = nil,
         color  = __rbtree_color_red,
     }
-    self.__header.lchild = self.__header
-    self.__header.rchild = self.__header
+    self.header.lchild = self.header
+    self.header.rchild = self.header
 end
 
 function rbtree:size()
-    return self.__count
+    return self.count
 end
 
 function rbtree:empty()
-    return self.__count == 0
+    return self:size() == 0
 end
 
 function rbtree:capacity()
@@ -298,28 +401,21 @@ end
 function rbtree:range(k)
 end
 
-function rbtree:insert_unique(x)
+function rbtree:insert_unique(v)
+    local r = __rbtree_get_insert_unique_pos(self, self.kextract(v))
+    local e = r[1]
+    local p = r[2]
+    if (p ~= nil) then
+        local z = __rbtree_insert_aux(self, p, v)
+        return {__rbtree_iterator:new(self, z), true }
+    else
+        return {__rbtree_iterator:new(self, e), false}
+    end
 end
 
-function rbtree:insert_equal(x)
+function rbtree:insert_equal(v)
+    local r = __rbtree_get_insert_equal_pos(self, self.kextract(v))
+    return __rbtree_iterator:new(self, __rbtree_insert_aux(self, r[2], v))
 end
 
-
-
-
-
-
-
-
-
-
-local __rbtree_node = newclass("__rbtree_node")
-
-function __rbtree_node:init()
-    self.__parent = nil
-    self.__lchild = nil
-    self.__rchild = nil
-    self.__color  = nil
-    self.__value  = nil
-end
-
+return rbtree
---
+++ test/container/rbtree.lua
@@ -0,0 +1,183 @@
+-------------------------------------------------------------------------------
+--- 'luamon.container.array'测试
+-------------------------------------------------------------------------------
+local TestSuite = require 'luamon.ltest'
+local RBTree    = require 'luamon.container.rbtree'
+
+-- 构建测试实例
+local mytest = TestSuite.new()
+
+-- 秩序检查
+local function is_sequential(mytree, unique)
+    local iter1 = mytree:xbegin()
+    local iter2 = iter1 + 1
+    while(iter2 ~= mytree:xend()) do
+        local v1 = iter1:get()
+        local v2 = iter2:get()
+        if not mytree.kcompare(mytree.kextract(v1), mytree.kextract(v2)) then
+            if unique then
+                return false
+            end
+            if mytree.kcompare(mytree.kextract(v2), mytree.kextract(v1)) then
+                return false
+            end
+        end
+        iter1 = iter2
+        iter2 = iter1 + 1
+    end
+    return true
+end
+
+-- 高度检查
+local function is_rbtree(mytree)
+
+    -- 获取节点高度
+    local function height(node)
+        local lheight = (node.lchild == nil) and 0 or height(node.lchild)
+        local rheight = (node.rchild == nil) and 0 or height(node.rchild)
+        return math.max(lheight, rheight) + ((node.color == 2) and 1 or 0)
+    end
+
+    -- 均衡检查
+    local function is_balance(node)
+        local cheight = height(node)
+        if (cheight <= 1) then
+            return true
+        else
+            local lheight = height(node.lchild)
+            local rheight = height(node.rchild)
+            if (lheight ~= rheight) then
+                return false
+            else
+                return is_balance(node.lchild) and is_balance(node.rchild) 
+            end
+        end
+    end
+
+    local root = mytree.header.parent
+    if (root == nil) then
+        return true
+    else
+        return is_balance(root)
+    end
+end
+
+-- 打印一颗树
+local function ptree(mytree)
+    -- 打印一行
+    local function pline(vtbl)
+        local ntbl = {}
+        local str  = ""
+        for _, v in ipairs(vtbl) do
+            if type(v) == "table" then
+                str = str .. string.format("{value = %s, color = %s}, ", v.value, v.color)
+                table.insert(ntbl, v.lchild or "nil")
+                table.insert(ntbl, v.rchild or "nil")
+            else
+                str = str .. v .. ", "
+            end
+        end
+        print(str)
+        if #ntbl > 0 then
+            pline(ntbl)
+        end
+    end
+    print("")
+    pline({ mytree.header.parent })
+end
+
+-- 插入测试
+function mytest.testA()
+
+    do
+        local mytree = RBTree:new()
+        for i = 1, 20 do
+            mytree:insert_unique(i)
+        end
+        mytest:assert_eq(mytree:size(), 20)
+        mytest:assert_true(is_sequential(mytree, "unique"))
+        mytest:assert_true(is_rbtree(mytree))
+    end
+
+    do
+        local mytree = RBTree:new()
+        for _, v in ipairs({45, 67, 12, 34, 56, 41, 75, 89, 13, 26, 62, 14, 15, 61, 64, 42, 47, 49, 50, 70}) do
+            mytree:insert_unique(v)
+        end
+        mytest:assert_eq(mytree:size(), 20)
+        mytest:assert_true(is_sequential(mytree, "unique"))
+        mytest:assert_true(is_rbtree(mytree))
+
+        local iter = mytree:xbegin()
+        mytest:assert_eq(iter:get(), 12)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 13)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 14)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 15)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 26)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 34)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 41)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 42)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 45)
+    end
+
+    do
+        local mytree = RBTree:new()
+        for _, v in ipairs({45, 67, 12, 34, 56, 47, 75, 89, 13, 26, 62, 14, 15, 61, 64, 42, 47, 49, 50, 12}) do
+            mytree:insert_equal(v)
+        end
+        mytest:assert_eq(mytree:size(), 20)
+        mytest:assert_false(is_sequential(mytree, "unique"))
+        mytest:assert_true (is_sequential(mytree          ))
+        mytest:assert_true (is_rbtree(mytree))
+
+        local iter = mytree:xbegin()
+        mytest:assert_eq(iter:get(), 12)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 12)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 13)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 14)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 15)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 26)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 34)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 42)
+        iter:advance(1)
+        mytest:assert_eq(iter:get(), 45)
+    end
+
+    do
+        local mytree = RBTree:new()
+        for _, v in ipairs({7121,3053,6114,2044,6126,5107,2047,6142,3075,7173,4108,9239,516,1032,7209,520,9319,1042,6218,6222,8368,526,6246,5235,1056,5243,7293,2116,5267,5271,7321,6302,3157,535,5299,7353,3169,9615,1079,7385,4320,6378,6386,1088,6394,9727,7437,4376,2192,1098,8776,4396,8800,2210,7497,2218,2220,8872,1113,2226,6498,7525,3257,9951,561,4480,2244,9999,5523,7573,6554,1130,1133,7597,3297,2276,9096,7637,6618,2290,1147,3319,3329,2310,4620,581,7713,146,8289,7733,6718,7757,588,8361,6746,295,3383,9432,6008,6778}) do
+            mytree:insert_unique(v)
+        end
+        mytest:assert_eq(mytree:size(), 100)
+        mytest:assert_true(is_sequential(mytree, "unique"))
+        mytest:assert_true(is_rbtree(mytree))
+    end
+
+    do
+        local mytree = RBTree:new()
+        for _, v in ipairs({137,192,215,138,162,2,81,58,208,133,71,196,66,105,154,128,103,16,29,242,263,296,156,85,54,108,132,256,205,236,116,42,128,31,180,290,33,260,48,240,94,119,137,160,223,291,287,27,7,16,269,269,12,126,55,66,233,187,23,138,123,138,180,250,169,61,241,202,22,288,143,115,108,279,275,31,270,262,58,276,278,27,246,290,153,2,57,86,188,79,224,11,216,105,261,86,166,202,287,187}) do
+            mytree:insert_equal(v)
+        end
+        mytest:assert_eq(mytree:size(), 100)
+        mytest:assert_false(is_sequential(mytree, "unique"))
+        mytest:assert_true (is_sequential(mytree))
+        mytest:assert_true(is_rbtree(mytree))
+    end
+end
+
+mytest:run()
