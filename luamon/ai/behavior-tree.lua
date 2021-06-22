26088537e6a92117345bfd9e2ea68269ca47470e
zlibrary <zlibrary@163.com>
2021-06-23 01:02:22 +0800

    1

--- luamon/ai/behavior-tree.lua
+++ luamon/ai/behavior-tree.lua
@@ -2,6 +2,77 @@
 --- 行为树框架，参考'https://github.com/BehaviorTree/BehaviorTree.cpp'实现
 -------------------------------------------------------------------------------
 require "luamon"
+local blackboard = require "luamon.ai.behavior-tree.blackboard"
+
+-- 生成行为树节点
+-- 1. 节点配置
+-- 2. 黑板对象
+-- 3. 子树集合
+local function make_behavior_node(nc, bb, subtrees)
+    local node = require(nc.modname):new(bb, nc.imports, nc.exports)
+    -- 注册监控器
+    if nc.observers then
+        for _, method in ipairs(nc.observers) do
+            node:subscribe(method)
+        end
+    end
+    -- 条件节点
+    if (node:type() == node.class.category.condition) then
+        return node
+    end
+    -- 行为节点
+    if (node:type() == node.clazz.category.action) then
+        return node
+    end
+    -- 控制节点
+    if (node:type() == node.class.category.control) then
+        for _, cc in ipairs(nc.children) do
+            node:add_child(make_behavior_node(cc, bb, subtrees))
+        end
+        return node
+    end
+    -- 修饰节点
+    if (node:type() == node.class.category.decorator) then
+        for _, cc in ipairs(nc.children) do
+            node:set_child(make_behavior_node(cc, bb, subtrees))
+        end
+        return node
+    end
+    -- 行为子树
+    if (node:type() == node.class.category.subtree) then
+        -- 构建子树黑板
+        local sb = blackboard:new(bb)
+        for k, v in pairs(nc.mapping or {}) do
+            sb:redirect(k, v)
+        end
+        -- 查找子树配置
+        local sc = subtrees[nc.subtree]
+        if (sc == nil) then
+            error(string.format("subtree[%s] undefined.", nc.subtree))
+        else
+            node:set_child(make_behavior_node(sc, sb, subtrees))
+        end
+        return node
+    end
+    -- 节点类型异常
+    error(string.format("module[%s] : unknown category.", nc.modname))
+end
 
 -------------------------------------------------------------------------------
---- 
+-- 行为树生成模块
+return
+{
+    create = function(config)
+        local mtree = config.maintree
+        if (mtree == nil) then
+            error("maintree is undefined.")
+        end
+        local maintree = assert(config.maintree, "maintree undefined.")
+        local sub
+
+
+        local trees = {}
+
+        
+    end
+}
--- test/ai/behavior-tree.lua
+++ test/ai/behavior-tree.lua
@@ -12,15 +12,77 @@ local sequence   = require 'luamon.ai.behavior-tree.controls.sequence'
 local mytest = TestSuite.new()
 
 
--- 巡逻行为模拟
+-- 巡逻模拟
 function mytest.testA()
 
-    local bb = blackboard:new()
-    local bt = fallback:new(bb)
+    -- local bb = blackboard:new()
+    -- local bt = fallback:new(bb)
+
+    -- -- 查找敌人
+    -- local lookup = action:new(function(this)
+    --     if (math.random(1, 99) <= 55) then
+    --         print("lookup failure")
+    --         return action.status.failure
+    --     else
+    --         print("lookup success")
+    --         this:set("coordinate", {1, 5})
+    --         return action.status.success
+    --     end
+    -- end, bb, {}, { coordinate = '{coordinate}' })
+
+    -- -- 攻击敌人
+    -- local attack = action:new(function(this)
+    --     local coordinate = this:get("coordinate")
+    --     if (coordinate == nil) then
+    --         print("attack error.")
+    --     else
+    --         print("attack ok", coordinate[1], coordinate[2])
+    --     end
+    --     return action.status.success
+    -- end, bb, { coordinate = '{coordinate}' }, {})
+
+    -- -- 顺序节点
+    -- local se = sequence:new(bb, {}, {})
+    -- se:add_child(lookup)
+    -- se:add_child(attack)
+
+    -- -- 自动巡逻
+    -- local patrol = action:new(function()
+    --     print("patrol")
+    --     if (math.random(1, 99) >= 33) then
+    --         return action.status.running
+    --     else
+    --         return action.status.success
+    --     end
+    -- end,bb, {}, {})
+
+    -- -- 注册节点
+    -- bt:add_child(se)
+    -- bt:add_child(patrol)
+
+    -- -- 执行相关行为
+    -- for i = 1, 10 do
+    --     bt:exec()
+    --     if (not bt:is_running()) then
+    --         bt:halt()
+    --     end
+    -- end
+
+end
+
+-- 子树模拟
+function mytest.testB()
+
+    -- 主干部分
+    local mbb = blackboard:new()
+    local mbt = fallback:new(mbb)
+    local cbb = blackboard:new(mbb)
+
+    cbb:redirect("mark", "target")
 
     -- 查找敌人
     local lookup = action:new(function(this)
-        if (math.random(1, 99) <= 55) then
+        if (math.random(1, 99) <= 44) then
             print("lookup failure")
             return action.status.failure
         else
@@ -28,7 +90,7 @@ function mytest.testA()
             this:set("coordinate", {1, 5})
             return action.status.success
         end
-    end, bb, {}, { coordinate = '{coordinate}' })
+    end, cbb, {}, { coordinate = '{coordinate}' })
 
     -- 攻击敌人
     local attack = action:new(function(this)
@@ -37,34 +99,36 @@ function mytest.testA()
             print("attack error.")
         else
             print("attack ok", coordinate[1], coordinate[2])
+            this:set("mark", "attach enemy.")
         end
         return action.status.success
-    end, bb, { coordinate = '{coordinate}' }, {})
+    end, cbb, { coordinate = '{coordinate}' }, { mark = '{mark}' })
 
     -- 顺序节点
-    local se = sequence:new(bb, {}, {})
+    local se = sequence:new(cbb, {}, {})
     se:add_child(lookup)
     se:add_child(attack)
 
     -- 自动巡逻
-    local patrol = action:new(function()
-        print("patrol")
+    local patrol = action:new(function(this)
+        print("patrol", this:get("mark") or "unknown")
         if (math.random(1, 99) >= 33) then
             return action.status.running
         else
+            this:set("mark", "reset")
             return action.status.success
         end
-    end,bb, {}, {})
+    end, mbb, {mark = "{target}"}, {mark = "{target}"})
 
     -- 注册节点
-    bt:add_child(se)
-    bt:add_child(patrol)
+    mbt:add_child(se)
+    mbt:add_child(patrol)
 
     -- 执行相关行为
-    for i = 1, 10 do
-        bt:exec()
-        if (not bt:is_running()) then
-            bt:halt()
+    for i = 1, 20 do
+        mbt:exec()
+        if (not mbt:is_running()) then
+            mbt:halt()
         end
     end
 
